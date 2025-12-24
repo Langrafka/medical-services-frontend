8787a0f3db333a6205caa61d2ac3e84d34d4d12b
kirky359 <lol783176@gmail.com>
2025-12-24 16:45:42 +0200

    feat: add husky pre-commit hooks for type checking

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
 export default function Home() {
-  const errorCheck: number = 12321;
-
-  console.log(errorCheck);
-
   return (
     <main className="flex min-h-screen items-center justify-center">
       <h1 className="text-3xl font-bold">Hello world!</h1>
