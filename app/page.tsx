4fb5a54eed49a124d65e139d89474c51841a0042
kirky359 <lol783176@gmail.com>
2025-12-24 16:35:25 +0200

    husky error check

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
 export default function Home() {
+  const errorCheck: number = 12321;
+
+  console.log(errorCheck);
+
   return (
     <main className="flex min-h-screen items-center justify-center">
       <h1 className="text-3xl font-bold">Hello world!</h1>
